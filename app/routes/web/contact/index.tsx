2bb8d89c1cd1dc121f013e69a08f0c66f08fd3c3
n2hq <n2hq.co@gmail.com>
2025-12-12 13:32:36 +0100

    Production: update commit

--- app/routes/web/contact/index.tsx
+++ app/routes/web/contact/index.tsx
@@ -1,11 +1,71 @@
 import React from 'react'
-import { appConfig } from '~/lib/lib'
+import { appConfig, generateRandom10DigitNumber, logError } from '~/lib/lib'
 import SearchLayoutMain from '~/routes/asset/SearchLayoutMain'
 import VerticalHeight from '~/routes/asset/VerticalHeight'
 import CallToActionSection from '~/components/content/CallToActionSection'
 import FooterSection from '~/routes/landing/assets/FooterSection'
 import FooterAlt from '~/components/footer/FooterAlt'
 import SearchLayout from '~/routes/asset/SearchLayout'
+import { LoaderFunction } from '@remix-run/node'
+import { MetaFunction } from '@remix-run/react'
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+
+    try {
+        let randomNumber
+        try {
+            randomNumber = generateRandom10DigitNumber()
+            //console.log(latestBusinesses)
+
+        } catch (error: any) {
+            console.log(error.message)
+        }
+
+        return {
+            randomNumber: randomNumber
+        }
+    } catch (err: any) {
+        logError(err)
+    }
+
+}
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+
+    let randomNo = data?.randomNumber
+    try {
+
+
+
+        return [
+            { title: "Bycet Contact - Online Business Directory, Explore Listings Around The World" },
+            { name: "description", content: "Discover and connect with businesses worldwide. Bycet.com helps you explore listings, find services, and grow your network across industries and countries." },
+            { name: "keywords", content: "Business Directory Service, Location Services" },
+            { property: "fb:app_id", content: "1325393508603168" },
+            { property: "og:url", content: "https://bycet.com/web/signup" },
+            { property: "og:type", content: "website" },
+            { property: "og:title", content: "Bycet Contact - Online Business Directory, Explore Listings Around The World" },
+            { property: "og:description", content: "Discover and connect with businesses worldwide. Bycet.com helps you explore listings, find services, and grow your network across industries and countries." },
+            { property: "og:image", content: `https://bycet.com/images/bycet.png?v=${randomNo}` },
+            { property: "og:image:secure_url", content: `https://bycet.com/images/bycet.png?v=${randomNo}` },
+            { property: "og:image:type", content: "image/png" },
+            { property: "og:image:width", content: "1200" },
+            { property: "og:image:height", content: "630" },
+            { property: "og:image:alt", content: "Bycet" },
+            { name: "twitter:site", content: "@bycetinc" },
+            { name: "twitter:creator", content: "@bycetinc" },
+            { name: "twitter:card", content: "summary_large_image" },
+            { name: "twitter:title", content: "Bycet Contact - Online Business Directory, Explore Listings Around The World" },
+            { name: "twitter:description", content: "Discover and connect with businesses worldwide. Bycet.com helps you explore listings, find services, and grow your network across industries and countries." },
+            { name: "twitter:image", content: `https://bycet.com/images/bycet.png?v=${randomNo}` },
+            { name: "twitter:image:alt", content: "Bycet Business Directory Logo" }
+        ];
+    } catch (e: any) {
+        logError(e)
+    }
+
+    return []
+};
 
 const index = () => {
     return (
--- app/routes/web/signin/index.tsx
+++ app/routes/web/signin/index.tsx
@@ -2,6 +2,67 @@ import React from 'react'
 import ResponsiveNav from '~/components/header/minimal/ResponsiveNav'
 import SigninBody from './assets/SigninBody'
 import { OperationProvider } from '~/context/OperationContext'
+import { generateRandom10DigitNumber, logError } from '~/lib/lib'
+import { MetaFunction } from '@remix-run/react'
+import { LoaderFunction } from '@remix-run/node'
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+
+    try {
+        let randomNumber
+        try {
+            randomNumber = generateRandom10DigitNumber()
+            //console.log(latestBusinesses)
+
+        } catch (error: any) {
+            console.log(error.message)
+        }
+
+        return {
+            randomNumber: randomNumber
+        }
+    } catch (err: any) {
+        logError(err)
+    }
+
+}
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+
+    let randomNo = data?.randomNumber
+    try {
+
+
+
+        return [
+            { title: "Bycet Sign in - Online Business Directory, Explore Listings Around The World" },
+            { name: "description", content: "Discover and connect with businesses worldwide. Bycet.com helps you explore listings, find services, and grow your network across industries and countries." },
+            { name: "keywords", content: "Business Directory Service, Location Services" },
+            { property: "fb:app_id", content: "1325393508603168" },
+            { property: "og:url", content: "https://bycet.com/web/signup" },
+            { property: "og:type", content: "website" },
+            { property: "og:title", content: "Bycet Sign in - Online Business Directory, Explore Listings Around The World" },
+            { property: "og:description", content: "Discover and connect with businesses worldwide. Bycet.com helps you explore listings, find services, and grow your network across industries and countries." },
+            { property: "og:image", content: `https://bycet.com/images/bycet.png?v=${randomNo}` },
+            { property: "og:image:secure_url", content: `https://bycet.com/images/bycet.png?v=${randomNo}` },
+            { property: "og:image:type", content: "image/png" },
+            { property: "og:image:width", content: "1200" },
+            { property: "og:image:height", content: "630" },
+            { property: "og:image:alt", content: "Bycet" },
+            { name: "twitter:site", content: "@bycetinc" },
+            { name: "twitter:creator", content: "@bycetinc" },
+            { name: "twitter:card", content: "summary_large_image" },
+            { name: "twitter:title", content: "Bycet Sign in - Online Business Directory, Explore Listings Around The World" },
+            { name: "twitter:description", content: "Discover and connect with businesses worldwide. Bycet.com helps you explore listings, find services, and grow your network across industries and countries." },
+            { name: "twitter:image", content: `https://bycet.com/images/bycet.png?v=${randomNo}` },
+            { name: "twitter:image:alt", content: "Bycet Business Directory Logo" }
+        ];
+    } catch (e: any) {
+        logError(e)
+    }
+
+    return []
+};
 
 const index = () => {
     return (
